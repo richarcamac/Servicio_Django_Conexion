b1027e275f5a042a8bd9fe2a22169050691bdeea
richarcamac <richi.rcp42@gmail.com>
2025-10-16 18:44:14 -0500

    Update urls.py

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('home/', views.home_view, name='home'),
+    path('recuperar/', views.recuperar_view, name='recuperar'),  # Ruta para recuperación de contraseña
 ]
+
 

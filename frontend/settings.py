2c7fb2c3c10cbeb4db48ecd5a60ca2ed2b8ec50d
richarcamac <richi.rcp42@gmail.com>
2025-10-18 11:18:14 -0500

    Refactor settings.py for improved organization

--- frontend/settings.py
+++ frontend/settings.py
@@ -124,7 +124,7 @@ CORS_ALLOW_ALL_ORIGINS = True  # Permitir peticiones externas (solo para pruebas
 # SENDGRID_ECHO_TO_STDOUT = True
 # DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "asiri.carrito@example.com")
 
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "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").strip()
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "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").strip()
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "asiri.carrito@example.com")
 
 # Configurar EMAIL_BACKEND de forma condicional:
@@ -138,6 +138,7 @@ else:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     SENDGRID_SANDBOX_MODE_IN_DEBUG = False
     SENDGRID_ECHO_TO_STDOUT = False
+
 
 
 

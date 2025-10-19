37fe15ef22863907a4a6266ec41cd476e8dbcc31
richarcamac <richi.rcp42@gmail.com>
2025-10-19 00:01:26 -0500

    Register Usuario and Producto models in admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Usuario, Producto
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('correo', 'nombre', 'rol', 'is_active', 'fecharegistro')
+    search_fields = ('correo', 'nombre')
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'precio', 'moneda', 'estado', 'fecharegistro', 'cantidad')
+    search_fields = ('titulo', 'descripcion')
+    list_filter = ('estado', 'moneda')

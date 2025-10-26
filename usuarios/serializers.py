9600e713b59c2d36ace05835bc3852b72226b40c
richarcamac <richi.rcp42@gmail.com>
2025-10-25 20:09:31 -0500

    Enhance ProductoSerializer with image URL method
    Add a method to return absolute URL for the image field.

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -2,7 +2,20 @@ from rest_framework import serializers
 from .models import Producto
 
 class ProductoSerializer(serializers.ModelSerializer):
+    imagen = serializers.SerializerMethodField()
+
     class Meta:
         model = Producto
         fields = '__all__'
 
+    def get_imagen(self, obj):
+        request = self.context.get('request')
+        if obj.imagen:
+            if hasattr(obj.imagen, 'url'):
+                url = obj.imagen.url
+            else:
+                url = str(obj.imagen)
+            if request is not None:
+                return request.build_absolute_uri(url)
+            return url
+        return None

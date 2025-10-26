0013d36e60d68aecb2a4bc9d0a0b7a0c24a83fa1
richarcamac <richi.rcp42@gmail.com>
2025-10-25 22:55:10 -0500

    Update serializers.py

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -8,20 +8,3 @@ class ProductoSerializer(serializers.ModelSerializer):
         model = Producto
         fields = '__all__'
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        request = self.context.get('request')
-        imagen_field = instance.imagen
-        if imagen_field:
-            # Si es un path, construye la URL absoluta
-            if hasattr(imagen_field, 'url'):
-                url = imagen_field.url
-            else:
-                url = str(imagen_field)
-            if request is not None:
-                representation['imagen'] = request.build_absolute_uri(url)
-            else:
-                representation['imagen'] = url
-        else:
-            representation['imagen'] = None
-        return representation

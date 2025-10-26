8adf82e08512eb75bb4f0b4bcc68874198a9ed70
richarcamac <richi.rcp42@gmail.com>
2025-10-25 21:52:44 -0500

    Update serializers.py

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -2,20 +2,22 @@ from rest_framework import serializers
 from .models import Producto
 
 class ProductoSerializer(serializers.ModelSerializer):
-    imagen = serializers.SerializerMethodField()
+    imagen = serializers.ImageField(required=False, allow_null=True)
 
     class Meta:
         model = Producto
         fields = '__all__'
 
-    def get_imagen(self, obj):
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
         request = self.context.get('request')
-        if obj.imagen:
-            if hasattr(obj.imagen, 'url'):
-                url = obj.imagen.url
-            else:
-                url = str(obj.imagen)
+        imagen_field = instance.imagen
+        if imagen_field:
+            url = imagen_field.url if hasattr(imagen_field, 'url') else str(imagen_field)
             if request is not None:
-                return request.build_absolute_uri(url)
-            return url
-        return None
+                representation['imagen'] = request.build_absolute_uri(url)
+            else:
+                representation['imagen'] = url
+        else:
+            representation['imagen'] = None
+        return representation
